9fdc2e0fc60681ad60c954a7425a05e6e0e62a48
sebfoothock <s.raemdonck@students.ephec.be>
2019-11-17 02:59:59 +0100

    16/11-4 maps

---
+++ js/maps.js
@@ -0,0 +1,17 @@
+// // Map options
+// var place = {
+//     center: {
+//         lat: 50.838387,
+//         lng: 4.391600
+//     }
+// }
+
+// var map = new google.maps.Map(document.getElementById('map'), {
+//     zoom: 15,
+//     center: place
+// });
+
+// var marker = new google.maps.Marker({
+//     position: place,
+//     map: map
+// });
