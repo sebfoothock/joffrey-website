d6ba9ecdf997aa7c250d31b702d2d0e42730dc78
sebfoothock <s.raemdonck@students.ephec.be>
2020-07-26 15:52:52 +0200

    change mail from

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -14,9 +14,16 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = "lemoinejoffrey@hotmail.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
 $subject = "Website Contact Form:  $name";
 $body = "You have received a new message from your website contact form.\n\n" . "Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
-$header = "From: lemoinejoffrey@hotmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$header = "From: seb180298@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Reply-To: $email";
 
 if (!mail($to, $subject, $body, $header)) {
     http_response_code(500);
+}
+
+$mail = mail ($to, "Subject: $email_subject",$message );
+if($mail){
+echo "Thank you for using our mail form";
+}else{
+echo "Mail sending failed.";
 }
